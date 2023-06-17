c244a54ec3ea172b04ee8a606e356278c1614762
Tran Phat <chouphat1506@gmail.com>
2023-06-17 10:23:03 +0700

    bundle fix

--- sources/server/webpack.config.js
+++ sources/server/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const webpackNodeExternals = require("webpack-node-externals")
 module.exports = {
-  entry: "./server.js", // Dẫn tới file index.js ta đã tạo
+  entry: {
+    bundle : "./server.js",
+  }, // Dẫn tới file index.js ta đã tạo
   output: {
     path: path.join(__dirname, "/build"), // Thư mục chứa file được build ra
-    filename: "bundle.js",// Tên file được build ra
-    publicPath: './dist/',
-    clean: true
+    filename: "[name].js",// Tên file được build ra
+    clean : true
   },
   target : 'node',
   node : {
@@ -20,6 +21,10 @@ module.exports = {
         test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
         exclude: /node_modules/, // Loại trừ thư mục node_modules
         use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
